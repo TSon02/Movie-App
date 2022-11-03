997ea14774a90ae7eab4f3c73727b989dca7f654
Son <vts27.06.02@gmail.com>
2022-11-03 13:31:32 +0700

    thư viện và cài đặt API css cơ bản

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <a href="/"> Click</a>
+            <iFrame src="https://www.youtube.com/watch?v=Lq-HMuxog3I" width="680" height="480" allowFullScreen></iFrame>
+        </div>
+    );
 }
 
 export default App;
